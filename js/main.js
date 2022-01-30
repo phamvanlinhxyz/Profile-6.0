f43f14ea29c050b5e2acfce6edd4e70696afee4f
Your Namephamvanlinhxyz <linh.pv194094@sis.hust.edu.vn>
2022-01-30 20:27:09 +0700

    format code

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 // Slick slide
 $(document).ready(function () {
-    $(".wrap").slick({
-        dots: true,
-        arrows: false,
-        infinite: true,
-        speed: 600,
-        slidesToShow: 1,
-        adaptiveHeight: true,
-    });
+  $(".wrapper").slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+  });
 });
 
 // Change cover fb
 const coverInput = document.getElementById("addCover");
 const coverImg = document.getElementById("coverImg");
 coverInput.addEventListener("change", (event) => {
-    URL.revokeObjectURL(coverImg.src);
-    const file = event.target.files[0];
-    file.preview = URL.createObjectURL(file);
-    coverImg.src = file.preview;
-    event.target.value = null;
+  URL.revokeObjectURL(coverImg.src);
+  const file = event.target.files[0];
+  file.preview = URL.createObjectURL(file);
+  coverImg.src = file.preview;
+  event.target.value = null;
 });
